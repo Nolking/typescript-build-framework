4cd1bcb7a713867e119a3cf82982c4c37ae5877d
Nolan <nolan.nn235@gmail.com>
2021-07-23 15:49:53 +0700

    add json server

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,30 @@
-import { User } from './models/User';
+// import { User } from './models/User';
 
-const user = new User({name: 'myname', age: 20});
+// const user = new User({name: 'myname', age: 20});
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+// console.log(user.get('name'));
+// console.log(user.get('age'));
 
-user.set({name:'newname'})
+// user.set({name:'newname'})
 
-user.on('change', () => {
-    console.log('change 1')
-});
-user.on('change', () => {
-    console.log('change 32')
+// user.on('change', () => {
+//     console.log('change 1')
+// });
+// user.on('change', () => {
+//     console.log('change 32')
     
-});
-user.on('save', () => {
-    console.log('save here triggered')
-});
+// });
+// user.on('save', () => {
+//     console.log('save here triggered')
+// });
 
-user.trigger('change')
+// user.trigger('change')
+
+import axios from 'axios';
+
+axios.post('http://localhost:3000/users', {
+    name: 'myname',
+    age: 20
+}); 
+
+axios.get('http://localhost:3000/users/1')
