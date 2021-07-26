c20e2831f724a2a9d91f7654427e040e9ec1ef3c
Nolan <nolan.nn235@gmail.com>
2021-07-26 18:54:17 +0700

    connect sync and user

---
+++ src/models/Attributes.ts
@@ -0,0 +1,25 @@
+import { UserProps } from "./User";
+
+
+export class Attributes<T> {
+    constructor(private data: T) {
+
+    }
+
+    get(propName: string): (number | string | boolean) {
+        return this.data[propName];
+    }
+
+    set(update: T): void {
+        Object.assign(this.data, update)
+    }
+
+}
+
+const attrs = new Attributes<UserProps>(
+    {id: 5, name: 'adsf', age: 20}
+);
+
+const id = attrs.get('id');
+
+type MatchData = [string, number, number];
